88867911d4777a05ae7296bc2a05d5c9af2417ee
Fulton Garcia <fulton@geniusplaza.com>
2019-11-15 19:11:30 -0500

    navbar design done

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Nav, NavItem } from './styles';
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <NavItem>Home</NavItem>
+      <NavItem>Fundy</NavItem>
+      <NavItem>Chin Li</NavItem>
+      <NavItem>Lux</NavItem>
+      <NavItem>Cash</NavItem>
+      <NavItem>Comet</NavItem>
+    </Nav>
+  )
+}
+
+export default Navbar;
---
+++ src/Components/Navbar/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Nav = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 2.77778rem;
+  align-items: center;
+  justify-content: space-evenly;
+  margin: 0px;
+  background-color: #8484ce;
+  position: fixed;
+`;
+
+export const NavItem = styled.li`
+  list-style-type: none;
+  color: white;
+  font-family: "Avenir Next", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"
+`;
