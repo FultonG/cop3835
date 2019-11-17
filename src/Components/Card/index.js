d40434d38338f060df906a797b15310775d2a740
Fulton Garcia <fultongarcia@gmail.com>
2019-11-17 12:12:07 -0500

    working mobile responsiveness with reusable card

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,14 +1,30 @@
 import styled from 'styled-components';
 
-const Card = styled.div(props => `
+const Card = styled.div(({width = '100%', height = 'auto', margin = '0px', lg = width, md = width, sm = width, xs= width}) => `
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   border-radius: 10px;
-  width: ${props.width ? props.width: '100%'}
-  height: ${props.height ? props.height: '100%'}
-  margin: ${props.margin ? props.margin: '0px'}
+  width: ${width}
+  height: ${height}
+  margin: ${margin}
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+  @media only screen and (max-width: 1200px){
+    width: ${lg}
+  }
+
+  @media only screen and (max-width: 992px){
+    width: ${md}
+  }
+
+  @media only screen and (max-width: 768px){
+    width: ${sm}
+  }
+
+  @media only screen and (max-width: 576px){
+    width: ${xs}
+  }
 `);
 
 export default Card;
--- src/Pages/home/index.js
+++ src/Pages/home/index.js
@@ -11,23 +11,23 @@ const Home = () => {
       <Card>
         <Title>Welcome To My Personal Petland</Title>
         <PetContainer>
-          <Card width={'15%'}>
+          <Card width={'15%'} margin={'5px'} xs={'100%'}>
             <Image src={FundyImg}></Image>
             <Title>Fundy</Title>
           </Card>
-          <Card width={'15%'}>
+          <Card width={'15%'} margin={'5px'} xs={'100%'}>
             <Image src={FundyImg}></Image>
             <Title>Chin Li</Title>
           </Card>
-          <Card width={'15%'}>
+          <Card width={'15%'} margin={'5px'} xs={'100%'}>
             <Image src={LuxImg}></Image>
             <Title>Lux</Title>
           </Card>
-          <Card width={'15%'}>
+          <Card width={'15%'} margin={'5px'} xs={'100%'}>
             <Image src={FundyImg}></Image>
             <Title>Cash</Title>
           </Card>
-          <Card width={'15%'}>
+          <Card width={'15%'} margin={'5px'} xs={'100%'}>
             <Image src={FundyImg}></Image>
             <Title>Comet</Title>
           </Card>
